bdcff4832df9e26113765de58cfc78e8ae0ce6f2
Mustafa Kibar <4563460+mkibar@users.noreply.github.com>
2022-07-26 13:56:16 +0300

    commit approximately mthod

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -73,11 +73,11 @@ suite('Unit Tests', function () {
       assert.isBelow(5 % 2, 2);
       assert.isBelow(2 / 3, 1);
     });
-    // // #10
-    // test('#approximately', function () {
-    //   assert.approximately(weirdNumbers(0.5), 1, 0);
-    //   assert.approximately(weirdNumbers(0.2), 1, 0);
-    // });
+    // #10
+    test('#approximately', function () {
+      assert.approximately(weirdNumbers(0.5), 1, 0.5);
+      assert.approximately(weirdNumbers(0.2), 1, 0.8);
+    });
   });
 
   // -----------------------------------------------------------------------------
